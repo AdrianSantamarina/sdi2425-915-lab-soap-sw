1ad08bce648749072f8e9baabdcbccc847964cf7
AdrianSantamarina <uo279062@uniovi.es>
2025-04-14 13:47:01 +0200

    SDI-2425-915-SW-11.1-Servicio-SOAP-Server.

---
+++ sdi-2425-915-lab-soap-server/src/main/java/es/uniovi/sdi/sdi2425915labsoapserver/Sdi2425915LabSoapServerApplication.java
@@ -0,0 +1,13 @@
+package es.uniovi.sdi.sdi2425915labsoapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2425915LabSoapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2425915LabSoapServerApplication.class, args);
+    }
+
+}
---
+++ sdi-2425-915-lab-soap-server/src/test/java/es/uniovi/sdi/sdi2425915labsoapserver/Sdi2425915LabSoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uniovi.sdi.sdi2425915labsoapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2425915LabSoapServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
